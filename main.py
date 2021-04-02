d78f2b24866540010aa2239c9202baaf1bd0f6ae
jakubek0708 <jakubek0708@gmail.com>
2021-04-02 20:02:45 +0200

    added democracy

--- main.py
+++ main.py
@@ -11,8 +11,16 @@ intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix = ',', intents=intents)
 
+#loading .env variables
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+LOG_CHANNEL = os.getenv('LOG_CHANNEL')
+BOT_SNOWFLAKE = os.getenv('BOT_SNOWFLAKE')
+PORT = int(os.getenv('PORT'))
+SERVER_IP = os.getenv('SERVER_IP')
+
 #minecraft server search
-server = MinecraftServer.lookup("smarkownia.ddns.net")
+server = MinecraftServer(SERVER_IP, PORT)
 
 
 
@@ -59,17 +67,14 @@ server = MinecraftServer.lookup("smarkownia.ddns.net")
 async def on_ready():
     print("czesc smark")
 
-#loading .env variables
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-LOG_CHANNEL = os.getenv('LOG_CHANNEL')
-BOT_SNOWFLAKE = os.getenv('BOT_SNOWFLAKE')
 
 #variables:
 log_channel = int(LOG_CHANNEL)
 bot_snowflake = int(BOT_SNOWFLAKE)
 slownik = {}
 who_joined = None
+test_channel = 768015589444419588
+send_verification_messages_ids = []
 
 @client.event
 async def on_member_join(member):
@@ -89,21 +94,23 @@ async def on_member_join(member):
 
     embed_send = await channel.send(embed=embed)
 
-
+    send_verification_messages_ids.append(embed_send.id)
     slownik[embed_send.id] = member
 
     await embed_send.add_reaction('✅')
     await embed_send.add_reaction('❌')
 
 
+emoji_list = ['<:SS:827162978662875188>', '<:S_:827163027157286923>', '<:A_:827163066289487902>', '<:B_:827163087370715166>', '<:C_:827163109415452713>', '<:D_:827163128155602944>']
 
-
+what_reaction_clicked = {}
 @client.event
 async def on_reaction_add(reaction, user):
-    channel = client.get_channel(log_channel)
-    pfp = who_joined.avatar_url_as(size=32)
 
-    if str(reaction.emoji) == '✅' and user.id != bot_snowflake:
+
+    if str(reaction.emoji) == '✅' and user.id != bot_snowflake and reaction.message.id in send_verification_messages_ids:
+        channel = client.get_channel(log_channel)
+        pfp = who_joined.avatar_url_as(size=32)
 
         var = discord.utils.get(user.guild.roles, name = "kumpel")
         await slownik[reaction.message.id].add_roles(var)
@@ -122,7 +129,9 @@ async def on_reaction_add(reaction, user):
         await embed_send.edit(embed=embed_verified)
 
 
-    if str(reaction.emoji) == '❌' and user.id != bot_snowflake:
+    if str(reaction.emoji) == '❌' and user.id != bot_snowflake and reaction.message.id in send_verification_messages_ids:
+        channel = client.get_channel(log_channel)
+        pfp = who_joined.avatar_url_as(size=32)
 
         await user.guild.kick(slownik[reaction.message.id])
 
@@ -138,6 +147,27 @@ async def on_reaction_add(reaction, user):
         embed_kicked.timestamp = datetime.datetime.utcnow()
 
         await embed_send.edit(embed=embed_kicked)
+
+    if str(reaction.emoji) in emoji_list and user.id != bot_snowflake and reaction.message.id in clicked:
+        channel = client.get_channel(test_channel)
+        if user not in clicked[reaction.message.id]:
+
+            print('clicked grape')
+            clicked[reaction.message.id].append(user)
+            what_reaction_clicked[user] = reaction
+        else:
+            print('clicked before')
+            await reaction.message.remove_reaction(reaction, user)
+
+    if self_vote_protection[reaction.message.id] == user.id and user.id != bot_snowflake and reaction.message.id in clicked:
+
+        await reaction.message.remove_reaction(reaction, user)
+
+@client.event
+async def on_reaction_remove(reaction, user):
+    print(user)
+    if str(reaction.emoji) in emoji_list and user.id != bot_snowflake and reaction.message.id in clicked and reaction == what_reaction_clicked[user]:
+        clicked[reaction.message.id].remove(user)
 
 
 @client.event
@@ -156,8 +186,8 @@ async def on_member_remove(member):
 
 @client.command()
 async def players(ctx):
-    status = server.status()
-    await ctx.send(status.players.online)
+    query = server.query()
+    await ctx.send(f'Na serwerze jest: {", ".join(query.players.names)}')
 
 
 @client.command()
@@ -168,5 +198,48 @@ async def fanfik(ctx):
 async def echo(ctx, *, message):
     await ctx.message.delete()
     await ctx.send(message)
+
+clicked = {}
+self_vote_protection = {}
+
+@client.command()
+async def vote(ctx):
+    global clicked
+    name_list = {'Smark': 293079974787678209,
+    'Spki': 414932696285052938,
+    'Desu': 350331531472011285,
+    'Mesimov': 293400827295563778,
+    'Celled': 172108539144306698,
+    'Pondoryna': 804418160186753025,
+    'Heryin': 265085929809510402,
+    'Witt': 226646760515305473,
+    'Toxic': 390956680705474571,
+    'Tadziu': 282421992051703809,
+    'Catto': 172108539144306698,
+    'Aamon': 269843173218582548,
+    'Awawa': 307541751248453632,
+    'Zoomek': 292634117084807168,
+    'Iza': 681541408074629120,
+    'Rayz': 564133511632781353,
+    'Ravioli': 437320877114261515,
+    'Ojciec': 263784520900280332,
+    'Moneyigos': 294936820595163142,
+    'Czesio': 535464141620510721,
+    'Igoruwa': 390926589640179712,
+    'Jager': 730209739736350770,
+    'Haav': 386209774481571841,
+    'Zneix': 288028423031357441,
+    'Stalowy': 327402104446648331,
+    'Wiktor': 475969701969788938}
+
+    for message in name_list:
+        vote_message = await ctx.send(message)
+
+        self_vote_protection[vote_message.id] = name_list[message]
+
+        for i in emoji_list:
+            await vote_message.add_reaction(i)
+
+        clicked[vote_message.id] = []
 
 client.run(TOKEN)

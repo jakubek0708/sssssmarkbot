3deb2bad61211a0f4c6560154456b41636b05d67
jakubek0708 <jakubek0708@gmail.com>
2021-04-10 11:31:34 +0200

    nice

--- main.py
+++ main.py
@@ -211,18 +211,6 @@ async def echo(ctx, *, message):
 clicked = {}
 self_vote_protection = {}
 
-@client.command()
-async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
-
-@client.command()
-async def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
-
 #@client.command()
 #async def vote(ctx):
 #    global clicked

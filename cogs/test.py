9dded21f77e2e8823039bde33199106f2b2b7dca
jakubek0708 <jakubek0708@gmail.com>
2021-04-10 11:31:48 +0200

    grappa ice

---
+++ cogs/test.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+
+class test(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.send("test failed damb")
+
+def setup(client):
+    client.add_cog(test(client))

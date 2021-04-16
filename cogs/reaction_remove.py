853cd6e0e0a786c863be84688861393687c96c4a
jakubek0708 <jakubek0708@gmail.com>
2021-04-16 18:24:52 +0200

    podejrzane

--- cogs/reaction_remove.py
+++ cogs/reaction_remove.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-from .reaction import troll_messages
 
 class reaction_remove(commands.Cog):
     def __init__(self, client):
@@ -9,7 +8,7 @@ class reaction_remove(commands.Cog):
 
     @commands.Cog.listener()
     async def on_reaction_remove(self, reaction, user):
-
+        from .reaction import troll_messages
         if str(reaction.emoji) == '<:sus:820070011070185503>' and user.id != bot_snowflake and reaction.message.id in troll_messages:
             await reaction.message.remove_reaction('<:trollcrazy:800421758099783710>', user.guild.get_member(bot_snowflake))
             troll_messages.remove(reaction.message.id)

70d97747be5ae8ac4bf6d9476361238dd66bc2c6
jakubek0708 <jakubek0708@gmail.com>
2021-04-16 19:00:01 +0200

    e

--- cogs/sus.py
+++ cogs/sus.py
@@ -10,6 +10,8 @@ class sus(commands.Cog): #that class is sussy
     async def on_message(self, message):
         if 'sus' in message.content.lower():
             await message.add_reaction('<:trollcrazy:800421758099783710>')
+        if message.content.lower() == 'e':
+            await message.add_reaction('<a:peepoHappyJAM:832655294500831295>')
 
 def setup(client):
     client.add_cog(sus(client))

0a54bcfa5f6420a485a0b4ad7f45a0813ac7c6d9
jakubek0708 <jakubek0708@gmail.com>
2021-04-18 13:44:36 +0200

    statsstatsats

--- cogs/smark.py
+++ cogs/smark.py
@@ -8,7 +8,7 @@ class smark(commands.Cog):
     async def smark(self, ctx):
         smark = self.client.get_user(293079974787678209)
         await ctx.send(ctx.message.author.nick)
-        await ctx.send(smark)
+        await ctx.send(smark.status)
 
 
 def setup(client):

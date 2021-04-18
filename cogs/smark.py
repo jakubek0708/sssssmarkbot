7e81bd14f84018e91406fddfbe2b835ed9585372
jakubek0708 <jakubek0708@gmail.com>
2021-04-18 13:52:38 +0200

    ssssss

--- cogs/smark.py
+++ cogs/smark.py
@@ -6,7 +6,8 @@ class smark(commands.Cog):
 
     @commands.command()
     async def smark(self, ctx):
-        smark = self.client.get_member(293079974787678209)
+         server = client.get_guild(id=489137343148851210)
+        smark = server.get_member(293079974787678209)
         await ctx.send(ctx.message.author.nick)
         await ctx.send(smark.status)
 

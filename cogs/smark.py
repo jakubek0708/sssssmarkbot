bb23c9819a75c314ee11be9c7985b8558d2e77ce
jakubek0708 <jakubek0708@gmail.com>
2021-04-19 10:36:19 +0200

    stan smarka

--- cogs/smark.py
+++ cogs/smark.py
@@ -6,11 +6,27 @@ class smark(commands.Cog):
 
     @commands.command()
     async def smark(self, ctx):
+        smark_status = 'nie żyje'
         server = self.client.get_guild(id=489137343148851210)
         smark = server.get_member(293079974787678209)
-        await ctx.send(smark.status)
         voice_state = smark.voice
-        await ctx.send(voice_state.channel.id == 831675365738545162)
+
+        if voice_state.channel.id == 831675365738545162:
+            smark_status = 'śpi'
+
+        elif smark.status == 'online':
+            smark_status = 'żyje'
+
+        elif smark.status == 'offline':
+            smark_status = 'nie żyje'
+
+        elif smark.status == 'idle':
+            smark_status = 'chyba nie żyje'
+
+        elif smark.status == 'dnd':
+            smark_status = 'coś robi cii'
+
+        await ctx.send(f'stan smarka: {smark_status}')
 
 def setup(client):
     client.add_cog(smark(client))

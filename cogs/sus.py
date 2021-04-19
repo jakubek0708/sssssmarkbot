33a8db0f3dc14c862fb47c04cfa0fcbfd28a9461
jakubek0708 <jakubek0708@gmail.com>
2021-04-19 10:46:52 +0200

    sussy fix

--- cogs/sus.py
+++ cogs/sus.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+sussy_messages = []
+
 class sus(commands.Cog): #that class is sussy
 
     def __init__(self, client):
@@ -10,6 +12,8 @@ class sus(commands.Cog): #that class is sussy
     async def on_message(self, message):
         if 'sus' in message.content.lower():
             await message.add_reaction('<:trollcrazy:800421758099783710>')
+            sussy_messages.append(message.id)
+
         if message.content.lower() == 'e':
             await message.add_reaction('<a:peepoHappyJAM:832655294500831295>')
 

fcd017df88e0e3437a1af384dc0d0c5f00f990e5
jakubek0708 <jakubek0708@gmail.com>
2021-04-20 21:05:40 +0200

    cząłsło

--- cogs/sus.py
+++ cogs/sus.py
@@ -3,19 +3,16 @@ from discord.ext import commands
 
 sussy_messages = []
 
-
-
 class sus(commands.Cog): #that class is sussy
 
     def __init__(self, client):
         self.client = client
 
-        self.smarkownia = self.client.get_guild(id=489137343148851210)
-
-        self.zmywara = discord.utils.get(self.smarkownia.roles, name = 'RANGA DZIEWCZYNA')
-
     @commands.Cog.listener()
     async def on_message(self, message):
+        smarkownia = self.client.get_guild(id=489137343148851210)
+
+        zmywara = discord.utils.get(smarkownia.roles, name = 'RANGA DZIEWCZYNA')
         if 'sus' in message.content.lower():
             await message.add_reaction('<:trollcrazy:800421758099783710>')
             sussy_messages.append(message.id)
@@ -23,7 +20,7 @@ class sus(commands.Cog): #that class is sussy
         if message.content.lower() == 'e':
             await message.add_reaction('<a:peepoHappyJAM:832655294500831295>')
 
-        if self.zmywara in message.author.roles:
+        if zmywara in message.author.roles:
             ctx = await self.client.get_context(message)
             await ctx.send('utkaj łeb zmywara')
 

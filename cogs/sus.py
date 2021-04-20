e46d852dc3fa5a41c361f7cc8b4760a5520e1790
jakubek0708 <jakubek0708@gmail.com>
2021-04-20 21:30:59 +0200

    incydent

--- cogs/sus.py
+++ cogs/sus.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 
 sussy_messages = []
 
+zmywara_time = None
+
 class sus(commands.Cog): #that class is sussy
 
     def __init__(self, client):
@@ -21,8 +23,14 @@ class sus(commands.Cog): #that class is sussy
             await message.add_reaction('<a:peepoHappyJAM:832655294500831295>')
 
         if zmywara in message.author.roles:
-            ctx = await self.client.get_context(message)
-            await ctx.send('utkaj łeb zmywara')
+            now = datetime.datetime.now()
+            if zmywara_time is not None:
+                if now > zmywara_time + datetime.timedelta(hours=1)
+                    ctx = await self.client.get_context(message)
+                    await ctx.send('utkaj łeb zmywara')
+                    zmywara_time = datetime.datetime.now()
+            else:
+                zmywara_time = datetime.datetime.now()
 
 def setup(client):
     client.add_cog(sus(client))

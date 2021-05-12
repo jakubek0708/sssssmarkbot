e7fa6782f96352ad3299c12e80db693f518e5033
jakubek0708 <jakubek0708@gmail.com>
2021-05-12 10:28:15 +0200

    seafes

--- cogs/wedaj.py
+++ cogs/wedaj.py
@@ -6,13 +6,11 @@ class es(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def wedajmiten(self, ctx):
+    async def dmsend(self, ctx, *, message):
         server = self.client.get_guild(id=489137343148851210)
         smark = server.get_member(293079974787678209)
-        await smark.send("Ej bo pracę domową mam z infy")
-        await smark.send("Mógłbyś ten?")
-        await smark.send("dać mi może? jak masz zrobioną")
-        await smark.send("no bo mi maszyna nie działa")
+        await smark.send(message)
+
         
 
 def setup(client):

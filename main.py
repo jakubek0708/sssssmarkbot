e74e5c51c34247a399c984ff320b56adf725d191
jakubek0708 <jakubek0708@gmail.com>
2021-06-20 18:06:59 +0200

    esa

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ client = commands.Bot(command_prefix=',,', intents=intents)
 
 @tasks.loop(seconds = 1)
 async def czarnuh():
-    channel = client.get_channel(560122060081463307)
-    await channel.send('nigger')
+    autism = client.get_channel(560122060081463307)
+    await autism.send('nigger')
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):

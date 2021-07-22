cf66dfb040d107a016a095ccac73c3787e7140f2
jakubek0708 <jakubek0708@gmail.com>
2021-07-22 16:45:53 +0200

    test

--- cogs/smarkbot_ai/smarkbot_ai.py
+++ cogs/smarkbot_ai/smarkbot_ai.py
@@ -1,6 +1,9 @@
 from discord.ext import commands
 import discord 
 
+append_string = ""
+last_user_id = None
+
 class smarkbot_ai(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -8,11 +11,18 @@ class smarkbot_ai(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author.id == 390956680705474571:
-            ctx = await self.client.get_context(message)
-            await ctx.send(message.content)
-            self.ai_training_list.append(message.content)
+        global append_string
+        global last_user_id
+        
+        if message.author.id in [390956680705474571, 293079974787678209]:
+            while message.author.id == last_user_id: 
+                append_string += message.content 
+            else:
+                self.ai_training_list.append(append_string)
             
+            append_string = message.content
+            last_user_id = message.author.id 
+
     @commands.command()
     async def show_list(self, ctx):
         await ctx.send(self.ai_training_list)

29d8a3b110fb914993d641bb3279171045e07506
jakubek0708 <jakubek0708@gmail.com>
2021-07-22 16:32:50 +0200

    test

--- cogs/smarkbot_ai/smarkbot_ai.py
+++ cogs/smarkbot_ai/smarkbot_ai.py
@@ -4,12 +4,19 @@ import discord
 class smarkbot_ai(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.ai_training_list = []
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.id == 390956680705474571:
             ctx = await self.client.get_context(message)
             await ctx.send(message.content)
+            self.ai_training_list.append(message.content)
+            
+    @commands.command()
+    async def show_list(self, ctx):
+        await ctx.send(self.ai_training_list)
+        
 
 def setup(client):
     client.add_cog(smarkbot_ai(client))

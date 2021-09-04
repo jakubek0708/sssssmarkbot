8c3c0e49cc7ac71532596d1d2f3bbc20f34fa29c
jakubek0708 <jakubek0708@gmail.com>
2021-09-04 21:20:35 +0200

    social score added hail china!

---
+++ cogs/wdcrl.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+import discord 
+
+class creditscore(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def creditscore(self, ctx, *, content):
+        data = list(content.split(" "))
+        if len(data) == 2:
+            await ctx.send('错误')
+        else: 
+            print(data, data[0], data[1])
+        
+
+def creditscore(client):
+    client.add_cog(creditscore(client))
